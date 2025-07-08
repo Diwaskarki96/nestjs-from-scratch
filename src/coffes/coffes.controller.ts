ea5afe5e821a256e43a1b94802fc7896e3e66290
Diwash Karki <diwaskarki96@gmail.com>
2025-07-08 18:13:00 +0000

    dynamic route: params id

--- src/coffes/coffes.controller.ts
+++ src/coffes/coffes.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffes')
 export class CoffesController {
-    @Get('flavours')
-    findAll(){
-        return "This is a get api that returns all the coffees"
-    }
-    @Get(':id')
-    findOne(){
-        return 'This actions return #[id]coffees'
-    }
-
+  @Get('flavours')
+  findAll() {
+    return 'This is a get api that returns all the coffees';
+  }
+  // @Get(':id')
+  // findOne(){
+  //     return 'This actions return #[id]coffees'
+  // }
+  @Get(':id')
+  findOne(@Param(':id') id: string) {
+    return `This action returns #${id} coffee`;
+  }
 }

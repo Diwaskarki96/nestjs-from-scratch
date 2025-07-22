f5f347f5061076019cc9f9706664cabf703ff645
Diwash Karki <diwaskarki96@gmail.com>
2025-07-22 07:10:07 -0500

    status code manupulate

--- src/coffes/coffes.controller.ts
+++ src/coffes/coffes.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('coffees')
 export class CoffesController {
-  @Get('flavours')
-  findAll() {
-    return 'This is a get api that returns all the coffees';
-  }
+ @Get()
+getAll(@Res() response) {
+  response.status(203).send('This is a get api that returns basic');
+}
+
+@Get('flavours')
+getFlavours() {
+  return 'This is a get api that returns all the coffees';
+}
   // @Get(':id')
   // findOne(){
   //     return 'This actions return #[id]coffees'
@@ -15,6 +21,7 @@ export class CoffesController {
     return `This action returns #${id} coffee`;
   }
   @Post()
+  @HttpCode(HttpStatus.GONE)
   create(@Body() body){
     return body;
   }

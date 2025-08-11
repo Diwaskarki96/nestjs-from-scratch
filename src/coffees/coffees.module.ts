0e5a57aaf5124074ed7d16790df9a3ddc1baa170
Diwash Karki <diwaskarki96@gmail.com>
2025-08-11 06:56:55 -0500

    ORM entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CoffesController } from 'src/coffes/coffes.controller';
 import { CoffeesService } from './coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entites/coffee.entity';
 
-@Module({controllers:[CoffesController], providers: [CoffeesService]})
+@Module({imports:[TypeOrmModule.forFeature([Coffee])] ,controllers:[CoffesController], providers: [CoffeesService]})
 export class CoffeesModule {}
+    

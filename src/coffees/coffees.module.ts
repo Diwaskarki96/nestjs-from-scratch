9e5560c7cea1d288da84e7b1dd2b54ec738aa36b
Diwash Karki <diwaskarki96@gmail.com>
2025-08-12 06:36:04 -0500

    Relational entites

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -3,7 +3,8 @@ import { CoffesController } from 'src/coffes/coffes.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entites/coffee.entity';
+import { Flavor } from './entites/flavor.entity';
 
-@Module({imports:[TypeOrmModule.forFeature([Coffee])] ,controllers:[CoffesController], providers: [CoffeesService]})
+@Module({imports:[TypeOrmModule.forFeature([Coffee,Flavor])] ,controllers:[CoffesController], providers: [CoffeesService]})
 export class CoffeesModule {}
     
--- src/coffees/entites/coffee.entity.ts
+++ src/coffees/entites/coffee.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Flavor } from "./flavor.entity";
 
 @Entity() // sql table ==='coffee'
 export class Coffee{
@@ -8,6 +9,9 @@ export class Coffee{
     name: string;
     @Column()
     brand: string;
-    @Column("json", {nullable:true })
+    // @Column("json", {nullable:true })
+    @JoinTable() // says “Make a new table in the database to store which coffee has which flavors”.
+    @ManyToMany //says “Coffee and Flavor have a many-to-many relationship”. 
+    (type => Flavor,(flavor)=>flavor.coffees) //"Each coffee can have many flavors, each flavor can be in many coffees, and store those links in a separate table."
     flavors: string[];
 } 
